c31062441471a3d592e02ed9750e7a6a9d4aa98d
muneendraadequate <puttamuneendraadequateinfosoft@gmail.com>
2026-09-23 11:30:34 +0530

    commit

---
+++ frontend/dist/assets/CalendarPage-CSxUV8ai.js
@@ -0,0 +1,1 @@
+import{u as t,j as n}from"./index-C-y5wCkd.js";import{a as s}from"./Calendar-BnGWJIai.js";function p(){const{data:a}=t();return n.jsx(s,{appointments:a.appointments})}export{p as default};
